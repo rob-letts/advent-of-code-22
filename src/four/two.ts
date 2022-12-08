2727ff4b0855f67b3d53c9e998e608ef8f80f503
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-12-08 23:59:53 +0000

    answer 4.2

---
+++ src/four/two.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs';
+
+function getData (path: string) {
+  const data = readFileSync(path, `utf-8`);
+  return data.toString().trim().split(`\n`);
+}
+
+const data = getData(`./src/four/resources/data.txt`);
+
+const overlaps = data.map(line => line.split(/,|-/).map(nums => Number(nums))).filter(pairs => {
+  const [startOne, endOne, startTwo, endTwo] = pairs;
+  return startOne <= endTwo &&  startTwo <= endOne;
+});
+
+console.log(overlaps.length);
